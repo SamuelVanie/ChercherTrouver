41878a07bc04f5e734d701a2ac40dcbc05f64075
SamuelVanie <vaniebi59@gmail.com>
2021-06-29 22:18:04 +0100

    Ajout des champs pour la SignUp des users

---
+++ src/accounts/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.forms import ModelForm
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
+
+
+class UserCustomForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username','email']
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields =['numero_de_tel', 'numero_de_chambre', 'genre']
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout
+from .forms import UserCustomForm, UserProfileForm 
 
 # Create your views here.
 def accounts_signup_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
+        user_form = UserCustomForm(request.POST, instance=request.user)
+        profile_form = UserProfileForm(request.POST, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form = user_form.save()
+            profile_form.save()
+            login(request, user_form)
             # return redirect('objects:objects_list') after I'll create my objects
     else:
-        form = UserCreationForm()
-    return render(request, 'accounts/accounts_signup.html', {'form':form})
+        user_form = UserCustomForm(request.POST, instance=request.user)
+        profile_form = UserProfileForm(request.POST, instance=request.user.profile)
+    return render(request, 'accounts/accounts_signup.html', {'user_form':user_form, 'profile_form':profile_form})
 
 
 def accounts_login_view(request):
